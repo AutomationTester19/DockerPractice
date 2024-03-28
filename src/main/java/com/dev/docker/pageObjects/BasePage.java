424725e6806826a4265df77335cd3e7de0fbf360
AutomationTester19 <Justypedigvijay@gmail.com>
2024-03-28 16:37:06 +0530

    first commit

---
+++ src/main/java/com/dev/docker/pageObjects/BasePage.java
@@ -0,0 +1,12 @@
+package com.dev.docker.pageObjects;
+
+public class BasePage {
+
+	public static void main(String[] args) {
+		
+		
+	   System.out.println("Welcome to Docker Learning");
+
+	}
+
+}
